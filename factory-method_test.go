358697e78c1a96c950daa7dbaf5bea8fdd3f4e2c
Erol Yapıcı <eyapici@ebay.com>
2020-04-12 00:29:55 +0300

    factory method

--- factory-method_test.go
+++ factory-method_test.go
@@ -16,3 +16,16 @@ func TestCreatePaymentMethodCash(t *testing.T) {
 	}
 	t.Log(msg)
 }
+
+func TestCreatePaymentMethodDebitCart(t *testing.T) {
+
+	payment, err := GetPaymentMethod(DebitCard)
+	if err != nil {
+		t.Fatal("Cache must exist")
+	}
+	msg := payment.Pay(5)
+	if msg != DebitCard {
+		t.Errorf("Payment method wasn't correct")
+	}
+	t.Log(msg)
+}
